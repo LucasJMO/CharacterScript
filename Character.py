1641196a1bff650a6a1b8fd76aad520047d10874
Lucas Marin <45007880+LucasJMO@users.noreply.github.com>
2020-05-16 15:15:01 -0400

    Initial Commit

---
+++ Character.py
@@ -0,0 +1,27 @@
+import json
+
+flist = []
+
+class Character:
+	# Ability Scores
+	_con = 0
+	_dex = 0
+	_str = 0
+	_cha = 0
+	_int = 0
+	_wis = 0
+	# Character Variables
+	_level = 0
+	_classes = []
+	_AC = 0
+	_hitpoints = 0
+	_fortitude = 0
+	_reflex = 0
+	_will = 0
+	_race = ""
+	_size = ""
+	_speed = ""
+	#def __init__(name=""):
+
+	def func():
+		print("Hello")
---
+++ MDtoJSON.py
@@ -0,0 +1,16 @@
+
+HD = 0
+BAB = 0
+FORT = 0
+REFL = 0
+WILL = 0
+SKILLS = 0
+SPECIAL = ""
+
+CHARACTER = ""
+
+def makeCharacter(mdFile):
+	print("do stuff")
+	
+def addLevel(tableRow):
+	
